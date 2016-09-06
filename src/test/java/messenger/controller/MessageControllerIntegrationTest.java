02a6be2ab29caeb81f9489c212bd36257ac5f70d
Aleksandr Shepeliev <a.shepeliev@gmail.com>
2016-09-07 00:23:07 +0300

    refactor tes using NoSQL Unit

--- src/test/java/messenger/controller/MessageControllerIntegrationTest.java
+++ src/test/java/messenger/controller/MessageControllerIntegrationTest.java
@@ -1,39 +1,42 @@
 package messenger.controller;
 
+import com.lordofthejars.nosqlunit.annotation.CustomComparisonStrategy;
+import com.lordofthejars.nosqlunit.annotation.IgnorePropertyValue;
+import com.lordofthejars.nosqlunit.annotation.ShouldMatchDataSet;
+import com.lordofthejars.nosqlunit.annotation.UsingDataSet;
+import com.lordofthejars.nosqlunit.mongodb.MongoDbRule;
+import com.lordofthejars.nosqlunit.mongodb.MongoFlexibleComparisonStrategy;
 import io.restassured.http.ContentType;
 import messenger.Application;
-import messenger.domain.Message;
 import messenger.model.MessageModel;
 import messenger.repository.MessageRepository;
 import messenger.service.MailService;
 import messenger.service.MessageService;
 import messenger.service.MessageServiceImpl;
-import org.hamcrest.number.OrderingComparison;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Date;
-
+import static com.lordofthejars.nosqlunit.mongodb.MongoDbConfigurationBuilder.mongoDb;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-import static org.springframework.data.mongodb.core.query.Query.query;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Application.class)
+@CustomComparisonStrategy(comparisonStrategy = MongoFlexibleComparisonStrategy.class)
+@IgnorePropertyValue(properties = {"time"})
 public class MessageControllerIntegrationTest {
 
-    @Autowired
-    private MongoOperations mongoOperations;
+    @Rule
+    public MongoDbRule remoteMongoDbRule = new MongoDbRule(mongoDb().databaseName("test").build());
+
+//    @Autowired
+//    private MongoOperations mongoOperations;
 
     @Autowired
     private MessageRepository messageRepository;
@@ -47,11 +50,13 @@ public class MessageControllerIntegrationTest {
     }
 
     @Test
+    @UsingDataSet(locations = "/message-initial.json")
+    @ShouldMatchDataSet(location = "/message-expected.json")
     public void send() throws Exception {
         // given
         MessageModel message = new MessageModel("subject", "message text");
         MessageController controller = new MessageController(messageService);
-        Date now = new Date();
+//        Date now = new Date();
 
         // @formatter:off
         given()
@@ -71,13 +76,13 @@ public class MessageControllerIntegrationTest {
         // @formatter:on
 
         // then
-        Query query = query(where("_id").is("1"));
-        Message actualMessage = mongoOperations.findOne(query, Message.class);
-
-        assertThat(actualMessage, notNullValue());
-        assertThat(actualMessage.getId(), is("1"));
-        assertThat(actualMessage.getSubject(), is("subject"));
-        assertThat(actualMessage.getMessage(), is("message text"));
-        assertThat(actualMessage.getTime(), OrderingComparison.greaterThan(now));
+//        Query query = Query.query(Criteria.where("_id").is("1"));
+//        Message actualMessage = mongoOperations.findOne(query, Message.class);
+//
+//        assertThat(actualMessage, notNullValue());
+//        assertThat(actualMessage.getId(), is("1"));
+//        assertThat(actualMessage.getSubject(), is("subject"));
+//        assertThat(actualMessage.getMessage(), is("message text"));
+//        assertThat(actualMessage.getTime(), OrderingComparison.greaterThan(now));
     }
 }
